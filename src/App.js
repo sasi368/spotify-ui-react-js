f51f9f411f78efd27f966ae0d60369a55cb33858
sasi kumar <sasimdu.murugan@gmail.com>
2021-06-05 18:12:22 +0530

    add some redux concept

--- src/App.js
+++ src/App.js
@@ -4,27 +4,38 @@ import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  //const [token, setToken] = useState(null);
+  const [{ user, token }, dispatch] = useDataLayerValue();
   //Run code based on a given conditions
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
+
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
 
       spotify.setAccessToken(_token);
       spotify.getMe().then((user) => {
-        console.log("user", user);
+        //console.log("user", user);
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
       });
     }
     console.log("I HAVE A TOKEN :", token);
   }, []);
-
+  //console.log(user);
+  console.log(token);
   return <div className="App">{token ? <Player /> : <Login />}</div>;
 }
 
