ce05b5cf2af7e0d42364cb248f7b72d4cf1ffa92
sasi kumar <sasimdu.murugan@gmail.com>
2021-06-06 09:39:49 +0530

    fix error in datalayer

--- src/DataLayer.js
+++ src/DataLayer.js
@@ -2,10 +2,10 @@ import React, { createContext, useContext, useReducer } from "react";
 
 export const DataLayerContext = createContext();
 
-export const DataLayer = ({ initialState, reducer, children }) => {
+export const DataLayer = ({ initialState, reducer, children }) => (
   <DataLayerContext.Provider value={useReducer(reducer, initialState)}>
     {children}
-  </DataLayerContext.Provider>;
-};
+  </DataLayerContext.Provider>
+);
 
 export const useDataLayerValue = () => useContext(DataLayerContext);
